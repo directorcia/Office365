271a1fb225657800db2efb2dcddc5212291d7422
Robert Crane <robertcrane@Robert-Cranes-MacBook-Air.local>
2018-06-30 15:24:03 +1000

    Import-module update

--- o365-connect-Exchange.ps1
+++ o365-connect-Exchange.ps1
@@ -1,3 +1,6 @@
+## CIAOPS
+## Script provided as is. Use at own risk. No guarantees or warranty provided.
+
 ## Variables
 $savedcreds=$false                      ## false = manually enter creds, True = auto
 $credpath = "c:\downloads\tenant.xml"   ## local file with credentials
@@ -7,6 +10,8 @@ clear-host
 ## set-executionpolicy remotesigned
 ## May be required once to allow ability to runs scripts in PowerShell
 
+## ensure that install-module msonline has been run
+## ensure that update-module msonline has been run to get latest module
 import-module msonline
 
 if ($savedcreds) {
@@ -19,5 +24,6 @@ else {
     connect-msolservice -credential $cred   
 }
 
+## Start Exchange Online session
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/?proxyMethod=RPS -Credential $Cred -Authentication Basic -AllowRedirection
 import-PSSession $Session
