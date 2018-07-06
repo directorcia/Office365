21492ce1b872d14b1cc0dfa83e45f5cc8142c0a9
Robert Crane <director@ciaops.com>
2018-07-07 09:57:32 +1000

    Select

--- o365-spo-extusr.ps1
+++ o365-spo-extusr.ps1
@@ -29,7 +29,7 @@ foreach ($site in $SiteCollections) ## Loop through all Site Collections in tena
 
 try {
     for ($i=0;;$i+=50) { ## There is a return limit of 50 users so need to capture data if more than 50 external users
-        Get-SPOExternalUser -SiteUrl $site.Url -PageSize 50 -Position $i -ea Stop | Select DisplayName,EMail,AcceptedAs,WhenCreated,InvitedBy,@{Name = "Url" ; Expression = { $site.url }}
+        Get-SPOExternalUser -SiteUrl $site.Url -PageSize 50 -Position $i -ea Stop | Select-object DisplayName,EMail,AcceptedAs,WhenCreated,InvitedBy,@{Name = "Url" ; Expression = { $site.url }}
     }
 }
 catch { ## this is where any error handling will appear if required
