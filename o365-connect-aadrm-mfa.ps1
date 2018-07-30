4affd4085257d03378cc849309e482a8c912580b
Robert Crane <director@ciaops.com>
2018-07-30 12:02:55 +1000

    aadrm

---
+++ o365-connect-aadrm-mfa.ps1
@@ -0,0 +1,26 @@
+## CIAOPS
+## Script provided as is. Use at own risk. No guarantees or warranty provided.
+
+## Description
+## Script designed to log into the Azure AD Rights Management module with MFA
+
+## Prerequisites = 0
+
+## Variables
+
+Clear-Host
+
+write-host -foregroundcolor green "Script started"
+
+## set-executionpolicy remotesigned
+## May be required once to allow ability to runs scripts in PowerShell
+
+## ensure that install-module aadrm has been run
+## ensure that update-module aadrm has been run to get latest module
+## https://www.powershellgallery.com/packages/AADRM/
+## Current version = 2.13.1.0, 3 May 2018
+
+
+## Connect to Azure AD Rights Management Service
+connect-aadrmservice
+write-host -foregroundcolor green "Now connected to the Azure AD Rights Management Service"
