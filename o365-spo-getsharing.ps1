580cb405aa279430ce8be915bab076945d928eef
Robert Crane <director@ciaops.com>
2018-08-03 12:39:06 +1000

    Formatting

--- o365-spo-getsharing.ps1
+++ o365-spo-getsharing.ps1
@@ -29,5 +29,6 @@ Write-Host
 Write-host -ForegroundColor Cyan "Getting all Sharepoint sites in tenant"
 
 get-sposite | Select-object url,sharingcapability
+Write-Host
 
 write-host -foregroundcolor Cyan "Script complete"
