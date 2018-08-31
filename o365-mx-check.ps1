06f7c59a15c97e7f6a025c0beb09ff6f9bf47b68
Robert Crane <director@ciaops.com>
2018-08-31 12:01:15 +1000

    comments

--- o365-mx-check.ps1
+++ o365-mx-check.ps1
@@ -27,45 +27,72 @@ write-host -ForegroundColor $systemmessagecolor "Start checking mailboxes"
 write-host
 foreach ($mailbox in $mailboxes){
     write-host -foregroundcolor yellow -BackgroundColor Black "Mailbox =",$mailbox.displayname
+
+## all mailboxes should have auditing enabled
+
     if ($mailbox.auditenabled) {
         write-host -foregroundcolor green "  Audit enabled =",$mailbox.AuditEnabled
     } else {
         write-host -foregroundcolor red "  Audit enabled =",$mailbox.AuditEnabled
     }
+
+## audit log limit for mailboxes shoudl be extended from default
+
     if ([timespan]::parse($mailbox.auditlogagelimit) -gt $auditlogagelimitdefault) {
         write-host -foregroundcolor green "  Audit login limit (days)",$mailbox.Auditlogagelimit
     } else {
         write-host -foregroundcolor red "  Audit login limit (days)",$mailbox.Auditlogagelimit
     }
+
+## all mailboxes should have their retained deleted item retention period extended to 30 days
+
     if ([timespan]::parse($mailbox.retaindeleteditemsfor) -gt $retaindeleteditemsfordefault) {
         write-host -foregroundcolor green "  Retain Deleted items for (days) =",$mailbox.retaindeleteditemsfor
     } else {
         write-host -foregroundcolor red "  Retain Deleted items for (days) =",$mailbox.retaindeleteditemsfor
     }
+
+## mailboxes should not be forwarding to oteh email addresses
+
     if ($mailbox.forwardingaddress -ne $null){
         write-host -foregroundcolor red "  Forwarding address =",$mailbox.forwardingaddress
     }
     if ($mailbox.forwardingsmtpaddress -ne $null){
         write-host -foregroundcolor red "  Forwarding SMTP address =",$mailbox.forwardingsmtpaddress
     }
+
+## mailboxes should have litigation hold enabled
+
     if ($mailbox.LitigationHoldEnabled) {
         write-host -foregroundcolor green "  Litigation hold =",$mailbox.litigationholdenabled
     } else {
         write-host -foregroundcolor red "  Litigation hold =",$mailbox.litigationholdenabled
     }
+
+## mailboxes should have archive enabled
+
     if ($mailbox.archivestatus -eq "active") {
         Write-host -foregroundcolor Green "  Archive status =",$mailbox.archivestatus
     } else {
         Write-host -foregroundcolor red "  Archive status =",$mailbox.archivestatus
     }
+
+## report mailbox maximum send and receive sizes
+
     Write-host "  Max send size =",$mailbox.maxsendsize
     write-host "  Max receive size =",$mailbox.maxreceivesize
     $extramailbox=get-casmailbox -Identity $mailbox.displayname
+
+## mailboxes should not have POP3 enabled
+
     if (-not $extramailbox.popenabled) {
         write-host -foregroundcolor green "  POP3 enabled =",$extramailbox.popenabled
     } else {
         write-host -foregroundcolor red "  POP3 enabled =",$extramailbox.popenabled
     }
+
+## mailboxes should not have IMAP enabled
+
     if (-not $extramailbox.ImapEnabled) {
         write-host -foregroundcolor green "  IMAP enabled =",$extramailbox.imapenabled
     } else {
