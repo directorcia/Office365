a282dd492f95ee9fc133fb1e449c499a95f05a43
Robert D. Crane <director@ciaops.com>
2019-01-07 20:19:20 +1100

    Update Elevation check

--- o365-update.ps1
+++ o365-update.ps1
@@ -12,26 +12,33 @@
 ## Variables
 $systemmessagecolor = "cyan"
 $processmessagecolor = "green"
+$errormessagecolor = "red"
+
 ## If you have running scripts that don't have a certificate, run this command once to disable that level of security
 ## set-executionpolicy -executionpolicy bypass -scope currentuser -force
 
 Clear-Host
 
-write-host -foregroundcolor $systemmessagecolor "Start Script"
+write-host -foregroundcolor $systemmessagecolor "Start Script`n"
 
-write-host -foregroundcolor $processmessagecolor "Update Azure AD module"
-Update-Module -Name AzureAD -force
-write-host -foregroundcolor $processmessagecolor "Update Azure AD Right Management module"
-Update-Module -Name AADRM -force
-write-host -foregroundcolor $processmessagecolor "Update Teams Module"
-Update-Module -Name MicrosoftTeams -Force
-write-host -foregroundcolor $processmessagecolor "Update SharePoint Online module"
-Update-Module -Name Microsoft.Online.SharePoint.PowerShell -force
-write-host -foregroundcolor $processmessagecolor "Update Microsoft Online module"
-Update-Module -Name MSOnline -force
-write-host -foregroundcolor $processmessagecolor "Update Azure module"
-Update-Module -name AzureRM -Force
-## New Az module
-## Update-Module -name Az -force
-
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+If ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){
+    write-host -foregroundcolor $processmessagecolor "Update Azure AD module"
+    Update-Module -Name AzureAD -force
+    write-host -foregroundcolor $processmessagecolor "Update Azure AD Right Management module"
+    Update-Module -Name AADRM -force
+    write-host -foregroundcolor $processmessagecolor "Update Teams Module"
+    Update-Module -Name MicrosoftTeams -Force
+    write-host -foregroundcolor $processmessagecolor "Update SharePoint Online module"
+    Update-Module -Name Microsoft.Online.SharePoint.PowerShell -force
+    write-host -foregroundcolor $processmessagecolor "Update Microsoft Online module"
+    Update-Module -Name MSOnline -force
+    write-host -foregroundcolor $processmessagecolor "Update Azure module"
+    Update-Module -name AzureRM -Force
+    ## New Az module
+    ## Update-Module -name Az -force
+}
+Else{
+     write-host -foregroundcolor $errormessagecolor "*** ERROR *** - Please re-run PowerShell environment as Administrator`n"
+}
 write-host -foregroundcolor $systemmessagecolor "Script completed`n"
