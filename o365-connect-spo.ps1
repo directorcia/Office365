4df966242db7f59818b24c4f39371f71cb456bf7
Robert Crane <director@ciaops.com>
2020-04-05 12:30:28 +1000

    Comments

--- o365-connect-spo.ps1
+++ o365-connect-spo.ps1
@@ -16,11 +16,11 @@ More scripts available by joining http://www.ciaopspatron.com
 ## Variables
 $systemmessagecolor = "cyan"
 $processmessagecolor = "green"
-$savedcreds=$false                      ## false = manually enter creds, True = from file
-$credpath = "c:\downloads\tenant.xml"   ## local file with credentials if required
+$savedcreds=$false                                      ## false = manually enter creds, True = from file
+$credpath = "c:\downloads\tenant.xml"                   ## local file with credentials if required
 ## Change <tenantname> to be your own tenant
-$tenant_input = $true                   ## change to false if you don't wish to be prompted for the tenant name
-$tenanturl= "https://<tenantname>-admin.sharepoint.com" ## SharePoint Admin URL for tenant if prompting not enabled
+$tenant_input = $true                                   ## change to false if you don't wish to have admin domain auto detected
+$tenanturl= "https://<tenantname>-admin.sharepoint.com" ## SharePoint Admin URL for tenant if auto detect not enabled
 
 ## If you have running scripts that don't have a certificate, run this command once to disable that level of security
 ## set-executionpolicy -executionpolicy bypass -scope currentuser -force
@@ -46,20 +46,22 @@ else {
 }
 
 ## Connect to Office 365 admin service
+write-host -foregroundcolor $processmessagecolor "Connecting to Office 365 Admin service"
 connect-msolservice -credential $cred
 write-host -foregroundcolor $processmessagecolor "Now connected to Office 365 Admin service"
 
-If ($tenant_input -eq $true) {
+## Auto detect SharePoint Online admin domain
+If ($tenant_input -eq $true) {                          ## if the auto detect option is set
     write-host -foregroundcolor $processmessagecolor "Determining SharePoint Administration URL"
-    $domains = get-msoldomain
-    foreach ($domain in $domains) {
-        if ($domain.name.contains('onmicrosoft')) {
-            $onname = $domain.name.split(".")
-            $tenantname = $onname[0]
-        }
-    }
-    $tenanturl = "https://" + $tenantname + "-admin.sharepoint.com"
-}
+    $domains = get-msoldomain                           ## get a list of all domains in tenant
+    foreach ($domain in $domains) {                     ## loop through all these domains
+        if ($domain.name.contains('onmicrosoft')) {     ## find the onmicrosoft.com domain
+            $onname = $domain.name.split(".")           ## split the onmicrosoft.com domain when found at the period. Will produce an array that contains each string as an element
+            $tenantname = $onname[0]                    ## the first string in this array is the name of the tenant
+        }                                               ## end of find the on.microsoft.com domain
+    }                                                   ## end of the domain checking look
+$tenanturl = "https://" + $tenantname + "-admin.sharepoint.com"
+}                                                       ## end of the auto check option
 
 Write-host -ForegroundColor $processmessagecolor "SharePoint admin URL =", $tenanturl
 
