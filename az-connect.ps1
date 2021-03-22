0f2fda1102708cb9e5d80e6e453196538e9817ef
Robert Crane <director@ciaops.com>
2021-03-22 14:10:24 +1100

    Update connect

--- az-connect.ps1
+++ az-connect.ps1
@@ -1,3 +1,8 @@
+param(                         
+    [switch]$noprompt = $false,   ## if -noprompt used then user will not be asked for any input
+    [switch]$noupdate = $false,   ## if -noupdate used then module will not be checked for more recent version
+    [switch]$debug = $false       ## if -debug create a log file
+)
 <# CIAOPS
 Script provided as is. Use at own risk. No guarantees or warranty provided.
 
@@ -18,39 +23,122 @@ set-executionpolicy -executionpolicy bypass -scope currentuser -force
 $systemmessagecolor = "cyan"
 $processmessagecolor = "green"
 $errormessagecolor = "red"
-$warnmessagecolor = "yellow"
-$version = "2.00"
+$warningmessagecolor = "yellow"
 
-start-transcript "..\az-connect.txt" | Out-Null                                        ## Log file created in parent directory that is overwritten on each run
-
-Clear-host
-
-write-host -foregroundcolor $systemmessagecolor "Script started. Version = $version`n"
-write-host -foregroundcolor cyan -backgroundcolor DarkBlue ">>>>>> Created by www.ciaops.com <<<<<<`n"
-write-host "--- Script to connect to an Azure tenant ---`n"
-
-if (get-module -listavailable -name az.accounts) {    ## Has the Azure PowerShell module been loaded?
-    write-host -ForegroundColor $processmessagecolor "Azure PowerShell module found"
-}
-else {              ## If Azure PowerShell module not found
-    write-host -ForegroundColor $warnmessagecolor -backgroundcolor $errormessagecolor "`n[001] - Azure PowerShell module not installed. Please install and re-run script`n"
-    write-host -ForegroundColor $warnmessagecolor -backgroundcolor $errormessagecolor "Exception message:",$_.Exception.Message,"`n"
-    Stop-Transcript                 ## Terminate transcription
-    exit 1                          ## Terminate script
+Clear-Host
+if ($debug) {
+    write-host "Script activity logged at ..\az-connect.txt"
+    start-transcript "..\az-connect.txt" | Out-Null                                        ## Log file created in parent directory that is overwritten on each run
 }
 
-write-host -ForegroundColor $processmessagecolor "[Start] - Connect to Azure tenant"
-try {
-    connect-AzAccount -warningaction "SilentlyContinue" | Out-Null
+write-host -foregroundcolor $systemmessagecolor "Azure Connection script started`n"
+write-host -ForegroundColor $processmessagecolor "Prompt =",(-not $noprompt)
+
+if (get-module -listavailable -name az.accounts) {    ## Has the Azure PowerShell module been installed?
+    write-host -ForegroundColor $processmessagecolor "Azure PowerShell module installed"
+}
+else {
+    write-host -ForegroundColor $warningmessagecolor -backgroundcolor $errormessagecolor "[001] - Azure PowerShell module not installed`n"
+    if (-not $noprompt) {
+        do {
+            $response = read-host -Prompt "`nDo you wish to install the Azure PowerShell module (Y/N)?"
+        } until (-not [string]::isnullorempty($response))
+        if ($result -eq 'Y' -or $result -eq 'y') {
+            write-host -foregroundcolor $processmessagecolor "Installing Azure PowerShell module - Administration escalation required"
+            Start-Process powershell -Verb runAs -ArgumentList "install-Module -Name az -Force -confirm:$false" -wait -WindowStyle Hidden
+            write-host -foregroundcolor $processmessagecolor "Azure PowerShell module installed"
+        }
+        else {
+            write-host -foregroundcolor $processmessagecolor "Terminating script"
+            if ($debug) {
+                Stop-Transcript | Out-Null                 ## Terminate transcription
+            }
+            exit 1                          ## Terminate script
+        }
+    }
+    else {
+        write-host -foregroundcolor $processmessagecolor "Installing Azure PowerShell module - Administration escalation required"
+        Start-Process powershell -Verb runAs -ArgumentList "install-Module -Name az -Force -confirm:$false" -wait -WindowStyle Hidden
+        write-host -foregroundcolor $processmessagecolor "Azure PowerShell module installed"    
+    }
+}
+
+if (-not $noupdate) {
+    write-host -foregroundcolor $processmessagecolor "Check whether newer version of Azure PowerShell module is available"
+    #get version of the module (selects the first if there are more versions installed)
+    $version = (Get-InstalledModule -name az) | Sort-Object Version -Descending  | Select-Object Version -First 1
+    #get version of the module in psgallery
+    $psgalleryversion = Find-Module -Name az | Sort-Object Version -Descending | Select-Object Version -First 1
+    #convert to string for comparison
+    $stringver = $version | Select-Object @{n='ModuleVersion'; e={$_.Version -as [string]}}
+    $a = $stringver | Select-Object Moduleversion -ExpandProperty Moduleversion
+    #convert to string for comparison
+    $onlinever = $psgalleryversion | Select-Object @{n='OnlineVersion'; e={$_.Version -as [string]}}
+    $b = $onlinever | Select-Object OnlineVersion -ExpandProperty OnlineVersion
+    #version compare
+    if ([version]"$a" -ge [version]"$b") {
+        Write-Host -foregroundcolor $processmessagecolor "Local module $a greater or equal to Gallery module $b"
+        write-host -foregroundcolor $processmessagecolor "No update required"
+    }
+    else {
+        Write-Host -foregroundcolor $warningmessagecolor "Local module $a lower version than Gallery module $b"
+        write-host -foregroundcolor $warningmessagecolor "Update recommended"
+        if (-not $noprompt) {
+            do {
+                $response = read-host -Prompt "`nDo you wish to update the Azure PowerShell module (Y/N)?"
+            } until (-not [string]::isnullorempty($response))
+            if ($result -eq 'Y' -or $result -eq 'y') {
+                write-host -foregroundcolor $processmessagecolor "Updating Azure PowerShell module - Administration escalation required"
+                Start-Process powershell -Verb runAs -ArgumentList "update-Module -Name azuread -Force -confirm:$false" -wait -WindowStyle Hidden
+                write-host -foregroundcolor $processmessagecolor "Azure PowerShell module - updated"
+            }
+            else {
+                write-host -foregroundcolor $processmessagecolor "Azure PowerShell module - not updated"
+            }
+        }
+        else {
+        write-host -foregroundcolor $processmessagecolor "Updating Azure PowerShell module - Administration escalation required" 
+        Start-Process powershell -Verb runAs -ArgumentList "update-Module -Name aipservice -Force -confirm:$false" -wait -WindowStyle Hidden
+        write-host -foregroundcolor $processmessagecolor "Azure PowerShell module - updated"
+        }
+    }
+}
+
+write-host -foregroundcolor $processmessagecolor "Azure PowerShell module loading"
+Try {
+    Import-Module az | Out-Null
 }
 catch {
-    write-host -ForegroundColor $warnmessagecolor -backgroundcolor $errormessagecolor "`n[002] - Unable to connect to Azure tenant`n"
-    write-host -ForegroundColor $warnmessagecolor -backgroundcolor $errormessagecolor "Exception message:",$_.Exception.Message,"`n"
-    Stop-Transcript                 ## Terminate transcription
-    exit 2                          ## Terminate script  
+    Write-Host -ForegroundColor $errormessagecolor "[002] - Unable to load Azure PowerShell module`n"
+    Write-Host -ForegroundColor $errormessagecolor $_.Exception.Message
+    if ($debug) {
+        Stop-Transcript | Out-Null                ## Terminate transcription
+    }
+    exit 2
 }
-## Select desired Azure subscription from list of subscriptions
-Get-AzSubscription -warningaction "SilentlyContinue" | Out-GridView -PassThru -title "Select the Azure subscription to use" | Select-AzSubscription | Out-Null
-write-host -ForegroundColor $processmessagecolor "[Finish] - Connect to Azure tenant"
+write-host -foregroundcolor $processmessagecolor "Azure PowerShell module loaded"
 
-write-host -foregroundcolor $systemmessagecolor "`nScript finished`n"
+## Connect to Azure AD service
+write-host -foregroundcolor $processmessagecolor "Connecting to Azure"
+try {
+    $result = Connect-AzAccount | Out-Null
+}
+catch {
+    Write-Host -ForegroundColor $errormessagecolor "[003] - Unable to connect to Azure`n"
+    Write-Host -ForegroundColor $errormessagecolor $_.Exception.Message
+    if ($debug) {
+        Stop-Transcript | Out-Null                 ## Terminate transcription
+    }
+    exit 3 
+}
+
+if (-not $noprompt) {
+    ## Select desired Azure subscription from list of subscriptions
+    Get-AzSubscription -warningaction "SilentlyContinue" | Out-GridView -PassThru -title "Select the Azure subscription to use" | Select-AzSubscription | Out-Null
+}
+
+write-host -foregroundcolor $processmessagecolor "Connected to Azure`n"
+write-host -foregroundcolor $systemmessagecolor "Azure Connection script finished`n"
+if ($debug) {
+    Stop-Transcript | Out-Null
+}
