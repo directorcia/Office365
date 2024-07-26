418ea9ba66c5e9a8effd7881ec35dbfc2c752f56
Robert Crane <director@ciaops.com>
2024-07-27 06:47:47 +1000

    New

---
+++ graph-globaladmins-get.ps1
@@ -0,0 +1,32 @@
+<# CIAOPS
+Script provided as is. Use at own risk. No guarantees or warranty provided.
+
+Description - Connect to the Microsoft Graph
+
+Source - https://github.com/directorcia/Office365/blob/master/graph-gloabladmins-get.ps1
+
+Prerequisites = 1
+1. Graph module installed
+
+More scripts available by joining http://www.ciaopspatron.com
+
+#>
+
+import-module Microsoft.Graph.Identity.DirectoryManagement
+
+Connect-MgGraph -Scopes "RoleManagement.Read.Directory","User.Read.All"
+
+$globalAdmins = Get-MgDirectoryRole | Where-Object { $_.displayName -eq "Global Administrator" }
+$globalAdminUsers = Get-MgDirectoryRoleMember -DirectoryRoleId $globalAdmins.id
+
+$globaladminsummary = @()
+foreach ($adminuser in $globalAdminUsers) {
+    $user = Get-MgUser -userId $adminuser.Id
+    $globaladminSummary += [pscustomobject]@{       
+        Id                = $adminuser.Id 
+        UserPrincipalName = $user.UserPrincipalName
+        DisplayName       = $user.DisplayName
+    }
+}
+
+$globaladminsummary
